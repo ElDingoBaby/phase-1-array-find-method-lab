6a267a2f4653fae834ca75c6286d6272f183fe21
Alexis Sanchez <alexiss574@gmail.com>
2023-02-20 17:15:27 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code your solution here
+
+
+//PRACTICE
+// function superbowlWin(array) {
+//     const win = array.find((object) => {
+//         return object.result === "W"
+//     })
+//     return win.year
+// }
+// console.log(superbowlWin(record))
+
+
+function superbowlWin(array) {
+    const winYear = []
+    for (const object of array) {
+        if (object.result === "W") {
+            winYear.push(object.year)
+        }
+    }
+    return winYear[0]
+}
+
+
+
+
+
+
+
+
+/// practice
+// function superbowlWin(array) {
+//     const winYears = []
+//     for (const object of array) {
+//         if (object.result === "W") {
+//             winYears.push(object.year)
+//         }
+//     }
+//     return winYears
+// }
+// console.log(superbowlWin(record))
